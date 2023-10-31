c05b6e7a0e0c56469e8e1990fbaf39f5568c13d3
Mark Hanna <mark.hanna@fawry.com>
2023-10-31 10:45:19 +0200

    update framework v 0.1.3

---
+++ FawryPaySDK.xcframework/ios-arm64_x86_64-simulator/FawryPaySDK_AVL.framework/Headers/FawryPaySDK_AVL.h
@@ -0,0 +1,18 @@
+//
+//  FawryPaySDK_AVL.h
+//  FawryPaySDK-AVL
+//
+//  Created by Milad Metias on 07/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FawryPaySDK_AVL.
+FOUNDATION_EXPORT double FawryPaySDK_AVLVersionNumber;
+
+//! Project version string for FawryPaySDK_AVL.
+FOUNDATION_EXPORT const unsigned char FawryPaySDK_AVLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FawryPaySDK_AVL/PublicHeader.h>
+
+
